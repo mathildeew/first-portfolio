10d106fce9d79321f6132e8a89f811591e258dc0
mathildeew <mathildewiik@hotmail.com>
2022-05-08 23:37:25 +0200

    dsgfhf

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const circle = document.getElementById("circle");
+const circleStyle = circle.style;
+
+document.addEventListener("mousemove", (e) => {
+  window.requestAnimationFrame(() => {
+    circleStyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
+    circleStyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
+  });
+});

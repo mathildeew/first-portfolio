de6ed483c901de5bc1119839a72f5f6a36adc112
mathildeew <mathildewiik@hotmail.com>
2022-10-03 23:28:12 +0200

    .

--- js/data/projects.js
+++ js/data/projects.js
@@ -2,32 +2,35 @@ export default [
   {
     id: 1,
     title: "Cerchio Studio / Project Exam 1",
-    description: "Bla bla bla",
-    image: src = ./images/cerchiostudio.png,
-    // "gitHubLink":;
-    // "webLink":,
+    description:
+      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
+    img: "../images/cerchiostudio.png",
+    gitHubLink: "#",
+    webLink: "#",
   },
   {
     id: 2,
     title: "Rainydays / Cross Course Project",
-    description: "Bla bla",
-    image: (src = "../images/rainydays.png"),
-    // "gitHubLink":;
-    // "webLink":,
+    description:
+      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
+    img: "../images/rainydays.png",
+    gitHubLink: "#",
+    webLink: "#",
   },
   {
-    // id: 3,
-    // title:
-    // description: "",
-    // "image":,
-    // "gitHubLink":;
-    // "webLink":,
+    id: 3,
+    title: "Community Sciene Museum / Semester Project 1",
+    description:
+      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
+    img: "../images/cms.png",
+    gitHubLink: "#",
+    webLink: "#",
   },
 ];
 
 // "id": ,
 //     "title": "",
 //     "description": "",
-//     "image":,
-//     "gitHubLink":;
-//     "webLink":,
+//     "img": "../images/"",
+//     "gitHubLink": "#",
+// "webLink": "#",
--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 import projects from "./data/projects.js";
 
 // CIRCLE POINTER
-const circle = document.getElementById("circle");
-const circleStyle = circle.style;
+// const circle = document.getElementById("circle");
+// const circleStyle = circle.style;
 
-document.addEventListener("mousemove", (e) => {
-  window.requestAnimationFrame(() => {
-    circleStyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
-    circleStyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
-  });
-});
+// document.addEventListener("mousemove", (e) => {
+//   window.requestAnimationFrame(() => {
+//     circleStyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
+//     circleStyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
+//   });
+// });
 
 // Display projects
-const projectsContainer = document.querySelector("#projects");
+const projectsContainer = document.querySelector(".projects");
 projects.forEach(function (project) {
-  projectsContainer += `
-                        <div class ="project">
-                          <img src=${project.image} alt=${project.title}/>
-                          <h3>${project.title}</h3>
-                          <p>${project.description}</p>
-                       `;
+  projectsContainer.innerHTML += `
+                                  <div class="project">
+                                    <img src=${project.img} alt=${projects.title}/>
+                                    <h3>${project.title}</h3>
+                                    <p>${project.description}</p>
+                                    <a href= "${project.webLink}">Visit website</a>
+                                    <a href= "${project.gitHubLink}">GitHub</a>
+                                  </div>
+                                 `;
 });

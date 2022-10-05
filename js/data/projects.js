b0395e5a7dd9871e91be06efcf2157efbf91db2e
mathildeew <mathildewiik@hotmail.com>
2022-10-05 15:05:26 +0200

    .

--- js/data/projects.js
+++ js/data/projects.js
@@ -5,8 +5,9 @@ export default [
     description:
       "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
     img: "../images/cerchiostudio.png",
-    gitHubLink: "#",
-    webLink: "#",
+    gitHubLink:
+      "https://github.com/Noroff-FEU-Assignments/project-exam-1-mathildeew.git",
+    webLink: "https://cerchio-studio.netlify.app/",
   },
   {
     id: 2,
@@ -14,8 +15,9 @@ export default [
     description:
       "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
     img: "../images/rainydays.png",
-    gitHubLink: "#",
-    webLink: "#",
+    gitHubLink:
+      "https://github.com/Noroff-FEU-Assignments/cross-course-project-mathildeew.git",
+    webLink: "https://rainydays-ccp.netlify.app/",
   },
   {
     id: 3,
@@ -23,8 +25,9 @@ export default [
     description:
       "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
     img: "../images/cms.png",
-    gitHubLink: "#",
-    webLink: "#",
+    gitHubLink:
+      "https://github.com/mathildeew/semester-project-1-community-science-museum.git",
+    webLink: "https://csm-oslo.netlify.app/",
   },
 ];
 

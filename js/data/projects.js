74605d7aff818d46a719b3501049a0924d739bd4
mathildeew <mathildewiik@hotmail.com>
2022-10-05 21:58:28 +0200

    Update projects.js

--- js/data/projects.js
+++ js/data/projects.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     title: "Cerchio Studio / Project Exam 1",
     description:
-      "This project is assigned by Cerchio Studio, a fictional ceramic studio. The final product is a blog site with its content stored as a Headless Content Management System. During this project the blog site has been designed, prototyped and built with HTML, CSS and JavaScript. The content has been fetched and displayed by making a call to the WordPress REST API. The final product has been tested with users, and changes was made accordingly to the feedback.",
+      "This project is assigned by Cerchio Studio, a fictional ceramic studio. The final product is a blog site with its content stored as a headless Content Management System.",
     img: "../images/cerchio-studio-screenshot.jpg",
     gitHubLink:
       "https://github.com/Noroff-FEU-Assignments/project-exam-1-mathildeew.git",
@@ -13,7 +13,7 @@ export default [
     id: 2,
     title: "Rainydays / Cross Course Project",
     description:
-      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
+      "This project was a part of several courses of the front-end development study program. The goal was to create an online shop selling men's and women's rain jackets.",
     img: "../images/rainydays-screenshot.jpg",
     gitHubLink:
       "https://github.com/Noroff-FEU-Assignments/cross-course-project-mathildeew.git",
@@ -23,7 +23,7 @@ export default [
     id: 3,
     title: "Community Sciene Museum / Semester Project 1",
     description:
-      "Thw website for Community Science Museum, a fictional museum. The brief was to make a informative and engaging website, that should be responsive and easy to use on a variety of devices.",
+      "The website for Community Science Museum, a fictional museum. The brief was to make a informative and engaging website, that should be responsive and easy to use on a variety of devices.",
     img: "../images/cms-screenshot.jpg",
     gitHubLink:
       "https://github.com/mathildeew/semester-project-1-community-science-museum.git",

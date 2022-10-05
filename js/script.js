ebb86313a62fe487c43b4e409c212c45872e94f7
mathildeew <mathildewiik@hotmail.com>
2022-10-05 17:43:24 +0200

    .

--- js/script.js
+++ js/script.js
@@ -24,3 +24,60 @@ projects.forEach(function (project) {
                                   </div>
                                  `;
 });
+
+// Hovering effects
+
+// HTML
+// const htmlLink = document.querySelector(".htmlHover");
+
+// htmlLink.addEventListener("mouseover", () => {
+//   htmlLink.innerHTML = `&#60;html&#62;`;
+//   htmlLink.style.fontFamily = "Source Sans Pro";
+//   htmlLink.style.textTransform = "lowercase";
+// });
+
+// htmlLink.addEventListener("mouseout", () => {
+//   htmlLink.innerHTML = `HTML`;
+//   htmlLink.style.fontFamily = "var(--text-font)";
+//   htmlLink.style.textTransform = "uppercase";
+// });
+
+// // CSS
+// const cssLink = document.querySelector(".cssHover");
+
+// cssLink.addEventListener("mouseover", () => {
+//   cssLink.style.color = "var(--pink)";
+//   cssLink.style.backgroundColor = "var(--burgundy)";
+// });
+
+// cssLink.addEventListener("mouseout", () => {
+//   cssLink.style.color = "var(--burgundy)";
+//   cssLink.style.backgroundColor = "var(--pink)";
+// });
+
+// //  Javascript
+// const jsLink = document.querySelector(".jsHover");
+
+// jsLink.addEventListener("mouseover", () => {});
+
+// jsLink.addEventListener("mouseout", () => {});
+
+// // Adobe XS
+// const xdLink = document.querySelector(".xdHover");
+
+// // Photoshop
+// const psLink = document.querySelector(".psHover");
+
+// // Illustrator
+// const illLink = document.querySelector(".illHover");
+
+// // Wordpress
+// const wpLink = document.querySelector(".wpHover");
+
+// wpLink.addEventListener("mouseover", () => {
+//   wpLink.style.rotate = "calc(180deg)";
+// });
+
+// wpLink.addEventListener("mouseout", () => {
+//   wpLink.style.rotate = "calc(0deg)";
+// });

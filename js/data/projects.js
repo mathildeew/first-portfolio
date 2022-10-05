e336c4e6dac25d114608b71daa68779416c27caa
mathildeew <mathildewiik@hotmail.com>
2022-10-05 19:04:42 +0200

    .

--- js/data/projects.js
+++ js/data/projects.js
@@ -3,8 +3,8 @@ export default [
     id: 1,
     title: "Cerchio Studio / Project Exam 1",
     description:
-      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
-    img: "../images/cerchiostudio.png",
+      "This project is assigned by Cerchio Studio, a fictional ceramic studio. The final product is a blog site with its content stored as a Headless Content Management System. During this project the blog site has been designed, prototyped and built with HTML, CSS and JavaScript. The content has been fetched and displayed by making a call to the WordPress REST API. The final product has been tested with users, and changes was made accordingly to the feedback.",
+    img: "../images/cerchio-studio-screenshot.jpg",
     gitHubLink:
       "https://github.com/Noroff-FEU-Assignments/project-exam-1-mathildeew.git",
     webLink: "https://cerchio-studio.netlify.app/",
@@ -14,7 +14,7 @@ export default [
     title: "Rainydays / Cross Course Project",
     description:
       "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
-    img: "../images/rainydays.png",
+    img: "../images/rainydays-screenshot.jpg",
     gitHubLink:
       "https://github.com/Noroff-FEU-Assignments/cross-course-project-mathildeew.git",
     webLink: "https://rainydays-ccp.netlify.app/",
@@ -23,8 +23,8 @@ export default [
     id: 3,
     title: "Community Sciene Museum / Semester Project 1",
     description:
-      "I'm baby raclette aesthetic fam schlitz DIY pok pok vibecession bushwick kombucha authentic raw denim marfa artisan hella pabst. Biodiesel venmo everyday carry DSA fixie kogi synth coloring book. Bodega boys portland helvetica ascot yuccie. Readymade mukbang kickstarter 3 wolf moon knausgaard everyday carry selfies four loko tacos paleo edison bulb ugh salvia banjo.",
-    img: "../images/cms.png",
+      "Thw website for Community Science Museum, a fictional museum. The brief was to make a informative and engaging website, that should be responsive and easy to use on a variety of devices.",
+    img: "../images/cms-screenshot.jpg",
     gitHubLink:
       "https://github.com/mathildeew/semester-project-1-community-science-museum.git",
     webLink: "https://csm-oslo.netlify.app/",
